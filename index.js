ce440bf37db71f5fe64b2032224796354b7c66d1
EugeneKurlovich <zhenikpggkurlovich@gmail.com>
2017-12-20 03:43:23 +0300

    POST /images

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer  = require('multer');
 const uuidv4 = require('uuid/v4');
+const sharp = require('sharp');
 
 
 const app = express();
@@ -11,6 +12,13 @@ const pdfStorage = multer.diskStorage({
   destination: './uploads/pdf/',
   filename: function (req, file, cb) {
       cb(null, uuidv4() + ".pdf");
+  }
+});
+
+const imageStorage = multer.diskStorage({
+  destination: './uploads/images/',
+  filename: function (req, file, cb) {
+      cb(null, uuidv4() + "-master" + file.originalname.slice(-4));
   }
 });
 
@@ -29,11 +37,29 @@ const pdfUpload = multer({
     }
 });
 
+const imageUpload = multer({
+  storage: imageStorage,
+  fileFilter: function fileFilter(req, file, cb) {
+      file.originalname.toUpperCase().endsWith(".JPG") || file.originalname.toUpperCase().endsWith(".PNG")
+          ? cb(null, true) : cb(null, false);
+  }
+});
+
 app.post('/upload', upload.single('file'), (req, res, next) => {
   res.json({ succeed: true });
 });
 
-
+app.post('/images', imageUpload.single('image'), async (req, res, next) => {
+  let regexp = /-master\.[^\.]+$/;
+  let filenames = [
+      req.file.filename.replace(regexp, "-preview" + req.file.filename.slice(-4)),
+      req.file.filename.replace(regexp, "-thumbnail" + req.file.filename.slice(-4)),
+      req.file.filename
+  ];
+  await sharp('./uploads/images/' + req.file.filename).resize(800, 600).toFile('./uploads/images/' + filenames[0]);
+  await sharp('./uploads/images/' + req.file.filename).resize(300, 180).toFile('./uploads/images/' + filenames[1]);
+  res.json(filenames);
+});
 
 app.post('/pdf', pdfUpload.array('files', 3), (req, res, next) => {
   let files = req.files;
